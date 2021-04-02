eff8c840146e6e1b5b3ac4a6a1040720925bad5c
Alexei <63992023+alexeipacholuk@users.noreply.github.com>
2021-04-02 10:29:58 -0600

    Add files via upload

---
+++ PacholukMidterm01/PacholukMidterm01/Form1.Designer.cs
@@ -0,0 +1,209 @@
+﻿
+namespace PacholukMidterm01
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.helpProvider1 = new System.Windows.Forms.HelpProvider();
+            this.asteroid01 = new System.Windows.Forms.PictureBox();
+            this.ufo = new System.Windows.Forms.PictureBox();
+            this.asteroid02 = new System.Windows.Forms.PictureBox();
+            this.asteroid03 = new System.Windows.Forms.PictureBox();
+            this.gameOverLabel = new System.Windows.Forms.Label();
+            this.scoreLabel = new System.Windows.Forms.Label();
+            this.hiScoreLabel = new System.Windows.Forms.Label();
+            this.restartButton = new System.Windows.Forms.Button();
+            this.quitButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.asteroid01)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ufo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.asteroid02)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.asteroid03)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // asteroid01
+            // 
+            this.asteroid01.BackColor = System.Drawing.Color.Transparent;
+            this.asteroid01.Image = global::PacholukMidterm01.Properties.Resources.asteroid01;
+            this.asteroid01.Location = new System.Drawing.Point(12, 162);
+            this.asteroid01.Name = "asteroid01";
+            this.asteroid01.Size = new System.Drawing.Size(89, 81);
+            this.asteroid01.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.asteroid01.TabIndex = 1;
+            this.asteroid01.TabStop = false;
+            // 
+            // ufo
+            // 
+            this.ufo.BackColor = System.Drawing.Color.Transparent;
+            this.ufo.Image = global::PacholukMidterm01.Properties.Resources.spaceship;
+            this.ufo.Location = new System.Drawing.Point(131, 739);
+            this.ufo.Name = "ufo";
+            this.ufo.Size = new System.Drawing.Size(89, 81);
+            this.ufo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ufo.TabIndex = 0;
+            this.ufo.TabStop = false;
+            // 
+            // asteroid02
+            // 
+            this.asteroid02.BackColor = System.Drawing.Color.Transparent;
+            this.asteroid02.Image = global::PacholukMidterm01.Properties.Resources.asteroid01;
+            this.asteroid02.Location = new System.Drawing.Point(348, 402);
+            this.asteroid02.Name = "asteroid02";
+            this.asteroid02.Size = new System.Drawing.Size(89, 81);
+            this.asteroid02.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.asteroid02.TabIndex = 1;
+            this.asteroid02.TabStop = false;
+            // 
+            // asteroid03
+            // 
+            this.asteroid03.BackColor = System.Drawing.Color.Transparent;
+            this.asteroid03.Image = global::PacholukMidterm01.Properties.Resources.asteroid01;
+            this.asteroid03.Location = new System.Drawing.Point(616, 739);
+            this.asteroid03.Name = "asteroid03";
+            this.asteroid03.Size = new System.Drawing.Size(89, 81);
+            this.asteroid03.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.asteroid03.TabIndex = 1;
+            this.asteroid03.TabStop = false;
+            // 
+            // gameOverLabel
+            // 
+            this.gameOverLabel.AutoSize = true;
+            this.gameOverLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.gameOverLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gameOverLabel.ForeColor = System.Drawing.Color.Cornsilk;
+            this.gameOverLabel.Location = new System.Drawing.Point(117, 281);
+            this.gameOverLabel.Name = "gameOverLabel";
+            this.gameOverLabel.Size = new System.Drawing.Size(481, 84);
+            this.gameOverLabel.TabIndex = 2;
+            this.gameOverLabel.Text = "GAME OVER";
+            this.gameOverLabel.Visible = false;
+            // 
+            // scoreLabel
+            // 
+            this.scoreLabel.AutoSize = true;
+            this.scoreLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.scoreLabel.ForeColor = System.Drawing.Color.Cornsilk;
+            this.scoreLabel.Location = new System.Drawing.Point(12, 9);
+            this.scoreLabel.Name = "scoreLabel";
+            this.scoreLabel.Size = new System.Drawing.Size(164, 37);
+            this.scoreLabel.TabIndex = 2;
+            this.scoreLabel.Text = "SCORE:  ";
+            // 
+            // hiScoreLabel
+            // 
+            this.hiScoreLabel.AutoSize = true;
+            this.hiScoreLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.hiScoreLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.hiScoreLabel.ForeColor = System.Drawing.Color.Cornsilk;
+            this.hiScoreLabel.Location = new System.Drawing.Point(117, 399);
+            this.hiScoreLabel.Name = "hiScoreLabel";
+            this.hiScoreLabel.Size = new System.Drawing.Size(278, 42);
+            this.hiScoreLabel.TabIndex = 2;
+            this.hiScoreLabel.Text = "HIGH SCORE: ";
+            this.hiScoreLabel.Visible = false;
+            // 
+            // restartButton
+            // 
+            this.restartButton.AutoSize = true;
+            this.restartButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.restartButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.restartButton.Location = new System.Drawing.Point(117, 565);
+            this.restartButton.Name = "restartButton";
+            this.restartButton.Size = new System.Drawing.Size(202, 62);
+            this.restartButton.TabIndex = 3;
+            this.restartButton.Text = "RESTART";
+            this.restartButton.UseVisualStyleBackColor = false;
+            this.restartButton.Visible = false;
+            this.restartButton.Click += new System.EventHandler(this.restartButton_Click);
+            // 
+            // quitButton
+            // 
+            this.quitButton.AutoSize = true;
+            this.quitButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.quitButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.quitButton.Location = new System.Drawing.Point(117, 671);
+            this.quitButton.Name = "quitButton";
+            this.quitButton.Size = new System.Drawing.Size(116, 62);
+            this.quitButton.TabIndex = 3;
+            this.quitButton.Text = "QUIT";
+            this.quitButton.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.quitButton.UseVisualStyleBackColor = false;
+            this.quitButton.Visible = false;
+            this.quitButton.Click += new System.EventHandler(this.quitButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(717, 965);
+            this.Controls.Add(this.quitButton);
+            this.Controls.Add(this.restartButton);
+            this.Controls.Add(this.scoreLabel);
+            this.Controls.Add(this.hiScoreLabel);
+            this.Controls.Add(this.gameOverLabel);
+            this.Controls.Add(this.asteroid03);
+            this.Controls.Add(this.asteroid02);
+            this.Controls.Add(this.asteroid01);
+            this.Controls.Add(this.ufo);
+            this.Name = "Form1";
+            this.Text = "Asteroid Field";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.asteroid01)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ufo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.asteroid02)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.asteroid03)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.HelpProvider helpProvider1;
+        private System.Windows.Forms.PictureBox ufo;
+        private System.Windows.Forms.PictureBox asteroid01;
+        private System.Windows.Forms.PictureBox asteroid02;
+        private System.Windows.Forms.PictureBox asteroid03;
+        private System.Windows.Forms.Label gameOverLabel;
+        private System.Windows.Forms.Label scoreLabel;
+        private System.Windows.Forms.Label hiScoreLabel;
+        private System.Windows.Forms.Button restartButton;
+        private System.Windows.Forms.Button quitButton;
+    }
+}
+
---
+++ PacholukMidterm01/PacholukMidterm01/Form1.cs
@@ -0,0 +1,224 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+//Alex Pacholuk
+//3-27-21
+
+//This program simulates flying through an asteroid field.
+//It speeds up as time goes on and stores the high score
+//in a txt file. Score is related to time alive.
+//The goal is to create a simple game that works with 
+//txt files to read and write high scores.
+//Use the arrow keys to move left and right.
+//the app uses the timer tool for updating position and score.
+//
+namespace PacholukMidterm01
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            gameOverLabel.Visible = false;
+            restartButton.Visible = false;
+            quitButton.Visible = false;
+
+        }
+
+        //txt file path
+        //CHANGE THIS BEFORE RUNNING ON DIFFERENT SYSTEM
+        string fileName = "D:/VisualStudioProjects/Midterm/PacholukMidterm01/HighScore.txt";
+
+        //asteroid speed
+        int asteroidSpeed = 1;
+
+        //time & score
+        int ticks = 0;
+
+        //timer for moving on screen elements
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //add to ticks
+            ticks += 1;
+
+            //if ticks is multiple of 1000
+            if(ticks==1000||ticks==2000||ticks==3000||ticks==4000||ticks==5000)
+            {
+                asteroidSpeed += 1;
+            }
+            else if(ticks==6000||ticks==7000||ticks==8000||ticks==9000||ticks==10000)
+            {
+                asteroidSpeed += 2;
+            }
+
+            //Store highest time in text file &
+            //Also speed up game everytime 
+            asteroid(asteroidSpeed);
+
+            //update score
+            scoreLabel.Text = "SCORE: " + ticks;
+
+            //check for game over
+            gameOver();
+
+            if (gameOverLabel.Visible == true)
+            {
+                //if new high score is true
+                if (newHighScore(fileName) == true)
+                {
+                    replaceHighScore(fileName, ticks);
+                }
+            }
+        }
+
+
+
+        //replaces high score in txt file
+        private void replaceHighScore(string fileName, int ticks)
+        {
+            //display new high score
+            hiScoreLabel.Text = "HIGH SCORE: " + ticks.ToString();
+
+            //rewrite txt file with new score
+            File.WriteAllText(fileName, ticks.ToString());
+        }
+
+
+        //checks for new high score
+        private Boolean newHighScore(string fileName)
+        {
+            try
+            {
+                string oldScore = File.ReadAllText(fileName);
+
+                if (ticks > int.Parse(oldScore))
+                {
+                    return true;
+                }
+                else
+                {
+                    hiScoreLabel.Text = "HIGH SCORE: " + oldScore;
+                    return false;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error reading file");
+                return false;
+            }
+        }
+
+
+        //Move asteroids
+        private void asteroid(int asteroidSpeed)
+        {
+            //new random
+            Random rnd = new Random();
+
+            //move asteroid 1 if below screen
+            if (asteroid01.Top >= 600)
+            {
+                //bring asteroid to top and randomly place along x axis
+                asteroid01.Top = 0;
+                asteroid01.Left = rnd.Next(0, 480);
+            }
+            else
+            {
+                asteroid01.Top += asteroidSpeed;
+            }
+
+            //move asteroid 2 if below screen
+            if (asteroid02.Top >= 600)
+            {
+                //bring asteroid to top and randomly place along x axis
+                asteroid02.Top = 0;
+                asteroid02.Left = rnd.Next(0, 480);
+            }
+            else
+            {
+                asteroid02.Top += asteroidSpeed;
+            }
+
+            //move asteroid 3 if below screen
+            if (asteroid03.Top >= 600)
+            {
+                //bring asteroid to top and randomly place along x axis
+                asteroid03.Top = 0;
+                asteroid03.Left = rnd.Next(0, 480);
+            }
+            else
+            {
+                asteroid03.Top += asteroidSpeed;
+            }
+
+        }
+
+
+
+        //Key down event
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            int moveLength = 10;
+            int frameMin = 0;
+            int frameMax = 500;
+            //if left key down
+            if (e.KeyCode == Keys.Left)
+            {
+                //if inbounds
+                if (ufo.Left > frameMin)
+                {
+                    //move left by moveLength
+                    ufo.Left += -moveLength;
+                }
+            }
+            //if right key down
+            if (e.KeyCode == Keys.Right)
+            {
+                //if inbounds
+                if (ufo.Right < (frameMax-20))
+                {
+                    //move rigth by moveLength
+                    ufo.Left += moveLength;
+                }
+            }
+        }
+
+        //Game Over
+        private void gameOver()
+        {
+            //if ufo intersects with asteroid
+            if (ufo.Bounds.IntersectsWith(asteroid01.Bounds)
+                ||ufo.Bounds.IntersectsWith(asteroid02.Bounds)
+                ||ufo.Bounds.IntersectsWith(asteroid03.Bounds))
+            {
+
+                //make game over labels/buttons visible
+                gameOverLabel.Visible = true;
+                hiScoreLabel.Visible = true;
+                restartButton.Visible = true;
+                quitButton.Visible = true;
+                timer1.Enabled = false;
+            }
+        }
+
+        private void quitButton_Click(object sender, EventArgs e)
+        {
+            //close game
+            this.Close();
+        }
+
+        private void restartButton_Click(object sender, EventArgs e)
+        {
+            //Restart game
+            Application.Restart();
+        }
+    }
+}
